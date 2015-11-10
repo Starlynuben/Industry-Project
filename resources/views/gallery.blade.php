e81191e9ab149e6f98e54444a96b1187aafdf324
unknown <benjamin.dorward@44389-WIN-501.auckland.natcoll.ac.nz>
2015-11-10 13:45:39 +1300

    3rd commit

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -27,17 +27,9 @@
 		
 	</div>
 </header>
-<div id="slideshow"></div>
 <body>
-	
-	<div id="companyMessage"></div>
 	<main>
 
-
-		<div class="boxHolder">
-			<div id="infoBox1"></div>
-			<div id="infoBox2"></div>
-		</div>
 
 	</main>
 </body>
