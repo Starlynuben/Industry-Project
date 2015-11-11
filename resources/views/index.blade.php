7df049a8347e7050eebb24582a33264aa555260b
unknown <benjamin.dorward@44389-WIN-501.auckland.natcoll.ac.nz>
2015-11-11 13:55:29 +1300

    4th commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,32 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>The Good Guys</title>
-	<link rel="stylesheet" href="../../resources/assets/css/styles.css">
-</head>
-<header>
-	<div id="headerContainer">
-		<div id="number"><p class="headerNumber">0800 448 988</p></div>
-		<div id="email"><p class="headerNumber">thegoodguys@biz.co.nz</p></div>
-	</div>
-	<div id="logo"></div>
-
-	<div>
-		<nav>
-			<ul>
-				<li><a href="index.blade.php">Home</a></li>
-				<li><a href="gallery.blade.php">Photos</a></li>
-				<li><a href="">Employment Vacancies</a></li>
-				<li><a href="">Contact</a></li>
-				<li><a href="">FAQ</a></li>
-			</ul>
-		</nav>
-	</div>
-	<div>
-		
-	</div>
-</header>
+@extends('template.main')  
+       
+@section('content') 
 <div id="slideshow"></div>
 <body>
 	
@@ -41,5 +15,6 @@
 
 	</main>
 </body>
-<footer></footer>
-</html>
+
+@end
+
